56779676e7bb495e99c9bb71858950057d5bd3cc
Prenshaw, David G <prenshawdg@ornl.gov>
2020-07-09 10:34:19 -0400

    Changing data source to pull global data. Still need population data

--- us-covid/src/store/CasesRedux.js
+++ us-covid/src/store/CasesRedux.js
@@ -41,65 +41,70 @@ export const actionCreators = {
         d3.csv('https://raw.githubusercontent.com/dprensha/covid19data/master/us-census-2019-est.csv', (data) => {
             populationData[data.UID] = data.Population;
         }),
-        d3.csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv', (data) => {
+        d3.csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv', (data) => {
+            console.log()
             const dates = Object.keys(data).filter(function(key) { return !isNaN(Date.parse(key)) });
-            if (Object.keys(allData.children).indexOf(data.Province_State) === -1) {
-                allData.children[data.Province_State] = {
+            if (Object.keys(allData.children).indexOf(data["Country/Region"]) === -1) {
+                allData.children[data["Country/Region"]] = {
                     UID: null,
                     x: [],
                     yConfirmed: [],
                     yDeaths: [],
                     yRecovered: [],
                     yActive: [],
-                    title: data.Province_State,
-                    navigableTitle: data.Province_State.replace(/[\.\W]/g,''),
+                    title: data["Country/Region"],
+                    navigableTitle: data["Country/Region"].replace(/[\.\W]/g,''),
                     parent: allData,
                     children: {},
                     population: 0
                 };
-                allData.children[data.Province_State].children[data.Admin2] = {
+                if(data["Province/State"] !== "") {
+                allData.children[data["Country/Region"]].children[data["Province/State"]] = {
                     UID: data.UID,
                     x: [],
                     yConfirmed: [],
                     yDeaths: [],
                     yRecovered: [],
                     yActive: [],
-                    title: data.Admin2,
-                    navigableTitle: data.Admin2.replace(/[\.\W]/g,''),
+                    title: data["Province/State"],
+                    navigableTitle: data["Province/State"].replace(/[\.\W]/g,''),
                     population: 0
                 };
+            }
                 
                 for(var j = 0; j < dates.length; j++) {
-                    allData.children[data.Province_State].x.push(dates[j]);
-                    allData.children[data.Province_State].yConfirmed.push(parseInt(data[dates[j]]));
-                    //allData.children[data.Province_State].yDeaths.push(parseInt(deathsRow[dates[j]]));
+                    allData.children[data["Country/Region"]].x.push(dates[j]);
+                    allData.children[data["Country/Region"]].yConfirmed.push(parseInt(data[dates[j]]));
+                    //allData.children[data["Country/Region"]].yDeaths.push(parseInt(deathsRow[dates[j]]));
 
-                    allData.children[data.Province_State].children[data.Admin2].x.push(dates[j]);
-                    allData.children[data.Province_State].children[data.Admin2].yConfirmed.push(parseInt(data[dates[j]]));
+                    if(data["Province/State"] !== "") {
+                    allData.children[data["Country/Region"]].children[data["Province/State"]].x.push(dates[j]);
+                    allData.children[data["Country/Region"]].children[data["Province/State"]].yConfirmed.push(parseInt(data[dates[j]]));
+                    }
                     if(allData.yConfirmed[j] !== undefined) {allData.yConfirmed[j] += (parseInt(data[dates[j]])) } else {allData.yConfirmed[j] = 0;};
-                    //allData.children[data.Province_State].children[data.Admin2].yDeaths.push(parseInt(deathsRow[dates[j]]));
+                    //allData.children[data["Country/Region"]].children[data["Province/State"]].yDeaths.push(parseInt(deathsRow[dates[j]]));
                 }
             }
 
-            else {
-                allData.children[data.Province_State].children[data.Admin2] = {
+            else if(data["Province/State"] !== "") {
+                allData.children[data["Country/Region"]].children[data["Province/State"]] = {
                     UID: data.UID,
                     x: [],
                     yConfirmed: [],
                     yDeaths: [],
                     yRecovered: [],
                     yActive: [],
-                    title: data.Admin2,
-                    navigableTitle: data.Admin2.replace(/[\.\W]/g,''),
+                    title: data["Province/State"],
+                    navigableTitle: data["Province/State"].replace(/[\.\W]/g,''),
                     population: 0
                 };
 
                 for(var j = 0; j < dates.length; j++) {
-                    allData.children[data.Province_State].yConfirmed[j] += (parseInt(data[dates[j]]));
+                    allData.children[data["Country/Region"]].yConfirmed[j] += (parseInt(data[dates[j]]));
                     if(allData.yConfirmed[j] !== undefined) {allData.yConfirmed[j] += (parseInt(data[dates[j]])) } else {allData.yConfirmed[j] = 0;};
-                    allData.children[data.Province_State].children[data.Admin2].x.push(dates[j]);
-                    allData.children[data.Province_State].children[data.Admin2].yConfirmed.push(parseInt(data[dates[j]]));
-                    //allData.children[data.Province_State].children[data.Admin2].yDeaths.push(parseInt(deathsRow[dates[j]]));
+                    allData.children[data["Country/Region"]].children[data["Province/State"]].x.push(dates[j]);
+                    allData.children[data["Country/Region"]].children[data["Province/State"]].yConfirmed.push(parseInt(data[dates[j]]));
+                    //allData.children[data["Country/Region"]].children[data["Province/State"]].yDeaths.push(parseInt(deathsRow[dates[j]]));
                 }
             }
         })
@@ -129,14 +134,14 @@ export const actionCreators = {
                         }
                     });
                     allData.children[sortedKeys[i]].children[sortedChildKeys[j]].yActive = allData.children[sortedKeys[i]].children[sortedChildKeys[j]].yConfirmed.map(function(yConfirmed, index) { return yConfirmed - allData.children[sortedKeys[i]].children[sortedChildKeys[j]].yRecovered[index] });
-                    allData.children[sortedKeys[i]].children[sortedChildKeys[j]].population = populationData[allData.children[sortedKeys[i]].children[sortedChildKeys[j]].UID];
-                    allData.children[sortedKeys[i]].children[sortedChildKeys[j]].yActivePerCapita = allData.children[sortedKeys[i]].children[sortedChildKeys[j]].yActive.map(function(yActive) { return yActive / allData.children[sortedKeys[i]].children[sortedChildKeys[j]].population });
+                    //allData.children[sortedKeys[i]].children[sortedChildKeys[j]].population = populationData[allData.children[sortedKeys[i]].children[sortedChildKeys[j]].UID];
+                    allData.children[sortedKeys[i]].children[sortedChildKeys[j]].yActivePerCapita = allData.children[sortedKeys[i]].children[sortedChildKeys[j]].yActive.map(function(yActive) { return yActive /*/ allData.children[sortedKeys[i]].children[sortedChildKeys[j]].population*/ });
 
-                    allData.children[sortedKeys[i]].population += parseInt(populationData[allData.children[sortedKeys[i]].children[sortedChildKeys[j]].UID]);
-                    allData.population += parseInt(populationData[allData.children[sortedKeys[i]].children[sortedChildKeys[j]].UID]);
+                    //allData.children[sortedKeys[i]].population += parseInt(populationData[allData.children[sortedKeys[i]].children[sortedChildKeys[j]].UID]);
+                    //allData.population += parseInt(populationData[allData.children[sortedKeys[i]].children[sortedChildKeys[j]].UID]);
                 }
 
-                allData.children[sortedKeys[i]].yActivePerCapita = allData.children[sortedKeys[i]].yActive.map(function(yActive) { return yActive / allData.children[sortedKeys[i]].population })
+                allData.children[sortedKeys[i]].yActivePerCapita = allData.children[sortedKeys[i]].yActive.map(function(yActive) { return yActive /*/ allData.children[sortedKeys[i]].population*/ })
 
                 allData.yRecovered = allData.yConfirmed.map(function(data, index) {
 					if (index < RECOVERY_PERIOD_DAYS) {
@@ -147,7 +152,7 @@ export const actionCreators = {
 					}
                 });
                 allData.yActive = allData.yConfirmed.map(function(yConfirmed, index) { return yConfirmed - allData.yRecovered[index] });
-                allData.yActivePerCapita = allData.yActive.map(function(yActive) { return yActive / allData.population });
+                allData.yActivePerCapita = allData.yActive.map(function(yActive) { return yActive /*/ allData.population*/ });
 
                 allData.x = allData.children[Object.keys(allData.children)[0]].x;
             }
