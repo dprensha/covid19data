98d2796ce50d5686ed51289f41feb8b3036ea256
dprensha <dprenshaw90@gmail.com>
2020-08-09 11:32:55 -0400

    Styling tooltip and adjusting color scheme

--- us-covid/src/components/Controls/LeafletMap/LeafletMap.jsx
+++ us-covid/src/components/Controls/LeafletMap/LeafletMap.jsx
@@ -9,6 +9,7 @@ import CanadaGeo from '../../../mapData/CanadaGeo.json';
 import ChinaGeo from '../../../mapData/ChinaGeo.json';
 import USCountyGeo from '../../../mapData/USCountyGeo.json';
 import FIPSToState from '../../../utilities/FIPSToState.json';
+import styles from './LeafletMap.module.scss';
 import './LeafletMap.css';
 
 const propTypes = {
@@ -53,27 +54,26 @@ class LeafletMap extends PureComponent {
     this.getStyle = this.getStyle.bind(this);
   }
 
-  getStyle = (property, FIPSLookup) => {
-    //console.log(property);
-    let currentEntity = this.props.entity.children[property.properties.DISPLAY_NAME];
+  getEntity = (property) => {
+    let entity = this.props.entity.children[property.properties.DISPLAY_NAME];
 
-    if(property.properties.NAME_0 === "Canada") {
-      currentEntity = this.props.entity.children["Canada"].children[property.properties.DISPLAY_NAME]
+    if (property.properties.NAME_0 === "Canada") {
+      entity = this.props.entity.children["Canada"].children[property.properties.DISPLAY_NAME]
     }
 
-    else if(property.properties.STUSPS) { //USState
-      currentEntity = this.props.usCases.children[property.properties.DISPLAY_NAME];
+    else if (property.properties.STUSPS) { //USState
+      entity = this.props.usCases.children[property.properties.DISPLAY_NAME];
     }
 
-    else if(property.properties.NAME_0 === "China") {
-      currentEntity = this.props.entity.children["China"].children[property.properties.DISPLAY_NAME]
+    else if (property.properties.NAME_0 === "China") {
+      entity = this.props.entity.children["China"].children[property.properties.DISPLAY_NAME]
     }
 
-    else if(property.properties.NAME_0 === "Australia") {
-      currentEntity = this.props.entity.children["Australia"].children[property.properties.DISPLAY_NAME]
+    else if (property.properties.NAME_0 === "Australia") {
+      entity = this.props.entity.children["Australia"].children[property.properties.DISPLAY_NAME]
     }
 
-    else if(property.properties.COUNTYFP) { //USCounty
+    else if (property.properties.COUNTYFP) { //USCounty
       const FIPSLookup = {
         "1": "Alabama",
         "2": "Alaska",
@@ -131,14 +131,20 @@ class LeafletMap extends PureComponent {
         "69": "Northern Mariana Islands",
         "72": "Puerto Rico",
         "78": "Virgin Islands"
-     };
-     if(this.props.usCases.children[FIPSLookup[parseInt(property.properties.STATEFP)]]){
-      currentEntity = this.props.usCases.children[FIPSLookup[parseInt(property.properties.STATEFP)]].children[property.properties.DISPLAY_NAME];
-     }
-     else {
-       console.log(property)
-     }
+      };
+      if (this.props.usCases.children[FIPSLookup[parseInt(property.properties.STATEFP)]]) {
+        entity = this.props.usCases.children[FIPSLookup[parseInt(property.properties.STATEFP)]].children[property.properties.DISPLAY_NAME];
+      }
+      else {
+        console.log(property)
+      }
     }
+
+    return entity;
+  }
+
+  getStyle = (property, FIPSLookup) => {
+    const currentEntity = this.getEntity(property);
 
     const activePerCapita = currentEntity && currentEntity.yActivePerCapita ? currentEntity.yActivePerCapita[currentEntity.yActivePerCapita.length - 1] * 1000 : 0
     let style = { fillColor: "blue", fillOpacity: "0" };
@@ -147,37 +153,68 @@ class LeafletMap extends PureComponent {
     const stateProvinceCountries = ["US", "Canada", "Australia", "China"]
 
     if ((this.state.dataLabel === "StateProvince" || this.state.dataLabel === "County") && stateProvinceCountries.includes(property.properties.DISPLAY_NAME)) {
-      
-    }
-    
-    else {
 
-      if (activePerCapita < stops[0]) {
+    }
+
+    else {
+      const breakpoint = .4;
+
+      if (activePerCapita < breakpoint * 1) {
         style = { fillColor: "#0000FF", fillOpacity: ".05" }
       }
-      else if (activePerCapita < stops[1]) {
+      else if (activePerCapita < breakpoint * 2) {
         style = { fillColor: "#0000FF", fillOpacity: ".1" }
       }
-      else if (activePerCapita < stops[2]) {
+      else if (activePerCapita < breakpoint * 3) {
         style = { fillColor: "#0000FF", fillOpacity: ".2" }
       }
-      else if (activePerCapita < stops[3]) {
+      else if (activePerCapita < breakpoint * 4) {
         style = { fillColor: "#0000FF", fillOpacity: ".3" }
       }
-      else if (activePerCapita < stops[4]) {
+      else if (activePerCapita < breakpoint * 5) {
         style = { fillColor: "#0000FF", fillOpacity: ".4" }
       }
-      else if (activePerCapita < stops[5]) {
+      else if (activePerCapita < breakpoint * 6) {
         style = { fillColor: "#0000FF", fillOpacity: ".5" }
       }
-      else if (activePerCapita < stops[6]) {
+      else if (activePerCapita < breakpoint * 7) {
         style = { fillColor: "#0000FF", fillOpacity: ".6" }
       }
-      else if (activePerCapita < stops[7]) {
-        style = { fillColor: "#0000FF", fillOpacity: ".73" }
+      else if (activePerCapita < breakpoint * 8) {
+        style = { fillColor: "#0000FF", fillOpacity: ".7" }
+      }
+      else if (activePerCapita < breakpoint * 9) {
+        style = { fillColor: "#0000FF", fillOpacity: ".8" }
+      }
+      else if (activePerCapita < breakpoint * 10) {
+        style = { fillColor: "#0000FF", fillOpacity: ".9" }
+      }
+      else if (activePerCapita < breakpoint * 11) {
+        style = { fillColor: "#B00000", fillOpacity: ".5" }
+      }
+      else if (activePerCapita < breakpoint * 12) {
+        style = { fillColor: "#B00000", fillOpacity: ".55" }
+      }
+      else if (activePerCapita < breakpoint * 13) {
+        style = { fillColor: "#B00000", fillOpacity: ".6" }
+      }
+      else if (activePerCapita < breakpoint * 14) {
+        style = { fillColor: "#B00000", fillOpacity: ".65" }
+      }
+      else if (activePerCapita < breakpoint * 15) {
+        style = { fillColor: "#B00000", fillOpacity: ".7" }
+      }
+      else if (activePerCapita < breakpoint * 16) {
+        style = { fillColor: "#B00000", fillOpacity: ".75" }
+      }
+      else if (activePerCapita < breakpoint * 17) {
+        style = { fillColor: "#B00000", fillOpacity: ".8" }
+      }
+      else if (activePerCapita < breakpoint * 18) {
+        style = { fillColor: "#B00000", fillOpacity: ".85" }
       }
       else {
-        style = { fillColor: "#0000FF", fillOpacity: ".86" }
+        style = { fillColor: "#B00000", fillOpacity: ".9" }
       }
     }
 
@@ -185,8 +222,10 @@ class LeafletMap extends PureComponent {
   };
 
   onClickThing = (event) => {
+    const entity = this.getEntity(event.layer.feature)
+    console.log(entity);
     this.setState({
-      selectedEntity: this.props.entity.children[event.layer.feature.properties.DISPLAY_NAME] ? this.props.entity.children[event.layer.feature.properties.DISPLAY_NAME] : {
+      selectedEntity: entity ? entity : {
         yActivePerCapita: [0],
         yConfirmed: [0],
         yActive: [0],
@@ -216,59 +255,80 @@ class LeafletMap extends PureComponent {
     }
   }
 
+  addThousandSeparators(value, formatMagnitude) {
+    if (formatMagnitude && Math.abs(Number(value)) >= 1.0e+6) {
+      return `${((Math.round(value / 1000)) / 1000).toFixed(2)} M`;
+    }
+    // else if(formatMagnitude && Math.abs(Number(value)) >= 1.0e+3) {
+    //     return `${(Math.round(value / 1.0e+3))} K`;
+    // }
+    else if (Math.abs(Number(value)) < 1000) {
+      return Math.round((value + Number.EPSILON) * 100) / 100;
+    }
+    else {
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+  }
+
+  formatPercentage(value) {
+    const percentage = Math.round((isNaN(value) ? 0 : value + Number.EPSILON) * 100) / 100;
+    return `${this.addThousandSeparators(percentage)}%`;
+  }
+
 
   render() {
     let tooltipContent = "No data available";
     if (this.state.selectedEntity && this.state.selectedEntity.yActive) {
-      tooltipContent = (
-        <table>
-          <tbody>
-            <tr>
-              <td colSpan="2" style={{ textAlign: "center" }}>{this.state.selectedEntity.title}</td>
-            </tr>
-            <tr>
-              <td>
-                Active Cases
+      const title =
+        tooltipContent = (
+          <table className={styles.popupTable}>
+            <tbody>
+              <tr>
+                <td colSpan="2" className={styles.popupTitle}>{this.state.selectedEntity.title}{this.state.selectedEntity.parent && this.state.selectedEntity.parent.title !== "World" ? `, ${this.state.selectedEntity.parent.title}` : ""}</td>
+              </tr>
+              <tr>
+                <td>
+                  Active Cases
                 </td>
-              <td>
-                {this.state.selectedEntity.yActive[this.state.selectedEntity.yActive.length - 1]}
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Active Cases Per 1,000
+                <td className={styles.popupValue}>
+                  {this.addThousandSeparators(this.state.selectedEntity.yActive[this.state.selectedEntity.yActive.length - 1], false)}
                 </td>
-              <td>
-                {this.state.selectedEntity.yActivePerCapita[this.state.selectedEntity.yActive.length - 1] * 1000}
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Total Cases
+              </tr>
+              <tr>
+                <td>
+                  Active Cases Per 1,000
                 </td>
-              <td>
-                {this.state.selectedEntity.yConfirmed[this.state.selectedEntity.yConfirmed.length - 1]}
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Deaths
+                <td className={styles.popupValue}>
+                  {this.addThousandSeparators(this.state.selectedEntity.yActivePerCapita[this.state.selectedEntity.yActive.length - 1] * 1000, false)}
                 </td>
-              <td>
-                {this.state.selectedEntity.yDeaths[this.state.selectedEntity.yDeaths.length - 1]}
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Mortality Rate
+              </tr>
+              <tr>
+                <td>
+                  Total Cases
                 </td>
-              <td>
-                {(this.state.selectedEntity.yDeaths[this.state.selectedEntity.yDeaths.length - 1]) / (this.state.selectedEntity.yConfirmed[this.state.selectedEntity.yConfirmed.length - 1]) * 100}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      )
+                <td className={styles.popupValue}>
+                  {this.addThousandSeparators(this.state.selectedEntity.yConfirmed[this.state.selectedEntity.yConfirmed.length - 1], true)}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Deaths
+                </td>
+                <td className={styles.popupValue}>
+                  {this.addThousandSeparators(this.state.selectedEntity.yDeaths[this.state.selectedEntity.yDeaths.length - 1])}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Mortality Rate
+                </td>
+                <td className={styles.popupValue}>
+                  {this.formatPercentage((this.state.selectedEntity.yDeaths[this.state.selectedEntity.yDeaths.length - 1]) / (this.state.selectedEntity.yConfirmed[this.state.selectedEntity.yConfirmed.length - 1]) * 100)}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        )
     }
 
     let data = WorldGeo;
